f7fbf199a73601febf3ed1d33421b32b738dcd6b
Mahihecker <l211896@lhr.nu.edu.pk>
2024-11-30 22:57:47 +0500

    updated loginapptest

--- Assignment 2/src/main/java/main/LoginService.java
+++ Assignment 2/src/main/java/main/LoginService.java
@@ -13,7 +13,7 @@ public class LoginService {
     public String authenticateUser(String email, String password) {
         String userName = null;
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-            String query = "SELECT name FROM User WHERE Email = ? AND Password = ?";
+            String query = "SELECT name FROM user WHERE Email = ? AND Password = ?";
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, email);
             stmt.setString(2, password);
